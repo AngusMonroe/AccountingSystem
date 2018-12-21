20440109ad25b24f393112c512acb0dfff2c5f89
Plvltra <331079072@qq.com>
2018-12-21 22:14:33 +0800

    Account build

---
+++ AccountingSystem/src/account/User.java
@@ -0,0 +1,35 @@
+package account;
+
+public class User {
+	
+	/** 0: Administrator 
+	 *  1: Seller
+	 *  2: Buyer
+	 *  3: Accountant  
+	 */
+	private int kind;
+	
+	public User(int kind) {
+		this.kind = kind;
+	}
+	
+	public void sellGoods(){
+		
+	}
+	
+	public void buyGoods(){
+		
+	}
+	
+	public void readItemTable(){
+		
+	}
+	
+	public void readTradeTable(){
+		
+	}
+	
+	public void readBalanceTable(){
+		
+	}
+}
--- AccountingSystem/src/program/Program.java
+++ AccountingSystem/src/program/Program.java
@@ -1,10 +1,20 @@
 package program;
 
+import javax.lang.model.type.IntersectionType;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Set;
+
+import account.User;
 import sql_connection.SqlConnection;
 import sql_connection.SqlConnection.*;
 
 public class Program
 {
+	private static User currUser;
+	private static SqlConnection sql;
+	
 	public static void main(String[] args)
 	{
 		try 
@@ -15,7 +25,7 @@ public class Program
 			String username = "ruangong";
 			String password = "ruangong";
 			
-			SqlConnection sql = new SqlConnection
+			sql = new SqlConnection
 					(
 						"jdbc:mysql://" + 
 						server +  ":" +
@@ -32,4 +42,39 @@ public class Program
 			System.out.println(e.getMessage());
 		}
 	}
+	
+	public static void Login(String name, String pwd) throws Exception {
+		int[] nameOK =  sql.where(Table.User, Column.Name, "'" + name + "'");
+		int[] pwdOK = sql.where(Table.User, Column.Password, "'" + pwd + "'");
+		int[] findAns = intersect(nameOK, pwdOK);
+		String[] kind = sql.select(Table.User, Column.Kind, findAns);
+		if(kind.length == 0){
+			throw new RuntimeException("用户名或密码错误");
+		}else{
+			int k = Integer.parseInt(kind[0]);
+			currUser = new User(k);
+		} 
+	}
+	
+	public static void Logout(){
+		currUser = null;
+	}
+	
+	public static int[] intersect(int[] a, int[] b){
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i = 0; i<a.length;i++){
+			for(int j = 0; j<b.length;j++){
+				if(a[i] == b[j]){
+					list.add(a[i]);
+				}
+			}
+		}
+		
+		int[] ans = new int[list.size()];
+		for(int i =0;i<ans.length;i++){
+			ans[i] = list.get(i);
+		}
+		return ans;
+	}
+	
 }
