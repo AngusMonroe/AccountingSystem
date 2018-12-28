be0936e2a5ea9fd1daa4121d748334ee28e43edc
guojiawei <guojiawei@sensetime-13.lan>
2018-12-28 15:17:06 +0800

    fix bug

--- Front-end/AccountingSystem/src/main.js
+++ Front-end/AccountingSystem/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import router from './router'
 import Toast from './util/Toast/Toast'
 import Connector from './util/ErpConnection'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false;
 
 Vue.use(Toast);
 Vue.use(Connector);
+Vue.use(VueResource);
 //require('./materialize');
 
   
